289582d188fcad560d56b39fc47ab4b4b7d6572f
Usifer10 <edouard.roby@student-cs.fr>
2023-05-07 19:27:55 +0200

    BetterBinTextureImplementation
    La texture apparait sur l'éditeur et n'est pas placée par le code

--- SeriousGameCS/Assets/Scripts/General/dev.cs
+++ SeriousGameCS/Assets/Scripts/General/dev.cs
@@ -12,30 +12,30 @@ public class dev : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Charger l'image depuis les ressources
-        Texture2D texture = Resources.Load<Texture2D>("Bins/yellowBin");
+        ////Charger l'image depuis les ressources
+        //Texture2D texture = Resources.Load<Texture2D>("Bins/yellowBin");
 
-        if (texture == null)
-        {
-            Debug.LogError("texture existe po");
-        }
-        Sprite sprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 25.0f, 0, SpriteMeshType.Tight);
-        Bin yellowBin = new Bin(sprite, Item.ItemType.Plastic);
-        Bins representation = yellowBinGO.AddComponent<Bins>();
-        yellowBinGO.AddComponent<SpriteRenderer>();
+        //if (texture == null)
+        //{
+        //    Debug.LogError("texture existe po");
+        //}
+        //Sprite sprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 25.0f, 0, SpriteMeshType.Tight);
+        //Bin yellowBin = new Bin(sprite, Item.ItemType.Plastic);
+        //Bins representation = yellowBinGO.AddComponent<Bins>();
+        //yellowBinGO.AddComponent<SpriteRenderer>();
 
-        yellowBinGO.name = "Yellow Bin";
+        //yellowBinGO.name = "Yellow Bin";
 
-        representation.Init(yellowBin);
-        SpriteRenderer yellowSpr = yellowBinGO.GetComponent<SpriteRenderer>();
-        yellowSpr.drawMode = SpriteDrawMode.Sliced;
-        yellowSpr.size = new Vector2(64.85f, 68.5f);
+        //representation.Init(yellowBin);
+        //SpriteRenderer yellowSpr = yellowBinGO.GetComponent<SpriteRenderer>();
+        //yellowSpr.drawMode = SpriteDrawMode.Sliced;
+        //yellowSpr.size = new Vector2(64.85f, 68.5f);
 
-        // Vérifier que l'image a bien été chargée
-        if (sprite == null)
-        {
-            Debug.LogError("Impossible de charger l'image");
-        }
+        //// Vérifier que l'image a bien été chargée
+        //if (sprite == null)
+        //{
+        //    Debug.LogError("Impossible de charger l'image");
+        //}
     }
 
     // Update is called once per frame
